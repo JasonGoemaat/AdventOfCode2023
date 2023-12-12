1456edf1c25f7f06b4486dd4fba10d81a5315877
Jason Goemaat <jason.goemaat@gmail.com>
2023-12-12 12:29:51 -0600

    Added launch.json for debugging
    Needed a new method to access the data files when debugging:
    
    ```py
    data_dir = os.path.dirname(__file__) + "/data/"
    test_solver(solve, data_dir+"test_day01b.input", '281')
    ```
    
    And to access util.js from either debugging or running from the script directory where 'util.py' is in the parent:
    
    ```py
    #sys.path.insert(0, os.getcwd()) # for debugging
    #sys.path.insert(0, os.getcwd() + '/..') # for running in terminal from script dir
    sys.path.insert(0, os.path.dirname(__file__) + "/../")
    ```

---
+++ _test/test_debug.py
@@ -0,0 +1,29 @@
+################################################################################
+# Testing debuging in VSCODE using launch.json
+
+import os
+print('cwd:')
+print(os.getcwd()) # ok, this is the base folder VSCODE has open
+
+# so try adding that to path to find util.py in it:
+import sys
+#sys.path.insert(0, os.getcwd()) # for debugging
+#sys.path.insert(0, os.getcwd() + '/..') # for running in terminal from script dir
+sys.path.insert(0, os.path.dirname(__file__) + "/../")
+
+print('sys.path is:')
+print(sys.path)
+print('sys.executable is:' + sys.executable)
+print('__file__ is: ', __file__)
+print('os.path.dirname(__file__) is: ', os.path.dirname(__file__))
+from util import do_puzzle, test_solver
+print('__name__ is: ' + __name__)
+data_dir = os.path.dirname(__file__) + "/data/"
+
+def solve(lines, out):
+  print('Hello, world!', file=out)
+  for i, line in enumerate(lines):
+    print('Line ' + str(i) + ': ' + line)
+  return '281'
+
+test_solver(solve, data_dir+"test_day01b.input", '281')
