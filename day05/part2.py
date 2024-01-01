3219e48f127ad355a63d95b730fa8a777181e1aa
Jason Goemaat <jason.goemaat@gmail.com>
2024-01-01 00:39:30 -0600

    Working on day 5 part 2

--- day05/part2.py
+++ day05/part2.py
@@ -8,82 +8,85 @@
 # we need to use ranges because a range may overlap, enclose, or
 # be outside of any of our mapping ranges.
 #
-# Looks like iterables might be a good fit for figuring this out, and I'd like
-# to learn: https://stackoverflow.com/questions/14099872/concatenating-two-range-function-results
-# That page shows how to create one:
-# def chain(*iterables):
-#     # chain('ABC', 'DEF') --> A B C D E F
-#     for it in iterables:
-#         for element in it:
-#             yield element
-# I'm not thinking of the seed locations to be iterable, but for ranges to
-# be iterable since they can change.   For an example if we have the range (5,10)
-# and apply mappings that will add 20 for 2-6 and add 40 for 7-15, the range
-# would then be split into two ranges: (25-26) and (47-50)
-# I don't know if more than one seed can be planted in a final location, but
-# since we're just returning the lowest location number we could then join
-# overlapping ranges also.
-# Ranges will be pythonesque, a tuple with start and end, where end is actually
-# the element AFTER the start (i.e. (5,11) means elements 5 through 10)
+# Ok, I settles on using the 'python-ranges' library (pip install python-ranges)
+# This lets you use Range and RangeSet objects.   I also create a
+# LocationMap class so I can associate a range with the difference
 
 import sys, os, re, bisect
 sys.path.insert(0, os.getcwd() + '/..')
 from util import test_solution
+from ranges import Range, RangeSet
+
+# not much here, most of it is __str__ and __repr__ for pretty debugging
+class LocationMap:
+  def __init__(self, nums):
+    self.source_range = Range(nums[1],nums[1]+nums[2])
+    self.diff = nums[0]-nums[1]
+  def __str__(self):
+     s = ""
+     if self.diff >=0:
+         s = "+"
+     return str(self.source_range)+s+str(self.diff)
+  def __repr__(self):
+     s = ""
+     if self.diff >=0:
+         s = "+"
+     return str(self.source_range)+s+str(self.diff)
 
 nums_re = re.compile(r"(\d+)")
 
-def map_ranges(maps, ranges):
-  pass
-
-def combine_ranges(ranges):
-  pass
-
 def parse_map(line):
-  vals = [int(x) for x in nums_re.findall(line)]
+  v = [int(x) for x in nums_re.findall(line)]
   # list is start, end (last num + 1 as python end is after last), change (amount to add to convert if the value is in the range)
-  return [vals[1], vals[1] + vals[2], vals[0] - vals[1]]
+  return LocationMap(v)
 
 def parse(lines):
-  seeds = [int(x) for x in nums_re.findall(lines[0])]
-  maps = []
+  seed_ranges = RangeSet()
+  seed_nums = [int(x) for x in nums_re.findall(lines[0])]
+  for i in range(0, len(seed_nums), 2):
+    a = seed_nums[i]
+    b = seed_nums[i+1]
+    seed_ranges += Range(a,a+b)
+  
+  map_types = []
   current = None
   for i in range(2,len(lines)):
     line = lines[i]
     if (line.endswith(' map:')):
-      current = {"name": line[0:(len(line)-5)], 'maps':[]}
-      maps.append(current)
+      current = []
+      map_types.append(current)
       continue
     if len(line) > 0:
-      current['maps'].append(parse_map(line))
-  return (seeds, maps)
-
-def apply_maps(seed, maps):
-  for map in maps:
-    old = seed
-    for m in map["maps"]:
-      if seed >= m[0] and seed < m[1]:
-        # print('Mapping',seed,'to',seed+m[2],'using',map['name'])
-        seed += m[2]
-        break
-    #print(map['name'],old,'-->',seed)      
-  return seed
+      current.append(parse_map(line))
+  return (seed_ranges, map_types)
 
 def output(seeds, maps, debug):
   print('Seeds:', seeds, file=debug)
-  for m in maps:
-    print('Map', m['name'], file=debug)
-    for m2 in m['maps']:
-      print('  ',m2[0],'to',m2[1]-1,'add',m2[2],'giving',m2[0]+m2[2],'to',m2[1]-1+m2[2], file=debug)
 
 def solve(lines,debug):
-  (seeds, maps) = parse(lines)
+  (seed_ranges, map_types) = parse(lines)
   # the actual result requires mapping all seeds, and returning the lowest
   # location number after applying all the maps
-  output(seeds, maps, debug)
-  locations = [apply_maps(s, maps) for s in seeds]
-  print(locations,file=debug)
-  return min(locations)
+  output(seed_ranges, map_types, debug)
+  for mt in map_types:
+    new_ranges = RangeSet()
+    for r in seed_ranges:
+      # try to apply each mapping inside this list of LocationMaps
+      for lm in mt:
+        if r != None:
+          i = RangeSet(r.intersection(lm.source_range))
+          d = RangeSet(r.difference(lm.source_range))
+          if i != None:
+            new_ranges += Range(min(i).start+lm.diff,min(i).end+lm.diff)
+            print('Intersection', r, 'lm', lm, 'i', i, 'd', d, 'new_ranges', new_ranges)
+            r = d
+      if r != None:
+        new_ranges += r
+        print('Unchanged', r, 'lm', lm, 'i', i, 'd', d, 'new_ranges', new_ranges)
+    seed_ranges = new_ranges
+  print(map_types,file=debug)
+  return min(seed_ranges).start
 
 # try solution on file(s)
-test_solution(solve, 'test.input', 35)
+test_solution(solve, 'test.input', 46)
 test_solution(solve, 'my.input')
